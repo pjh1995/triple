fc79d878823d5c84d2a9ab4b709bb4be32992275
jhpark <qkr13wlgus57@gmail.com>
2022-06-21 23:34:25 +0900

    feat(components): organisms add IntroBody

---
+++ src/components/organisms/intro/Body.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Achivements } from '@/components/molecules/intro';
+
+const Body: FC = () => {
+  return (
+        <Main>
+            <Achivements />
+        </Main>
+  );
+};
+
+export default Body
+
+const Main = styled.main`
+  height: 100vh
+  margin-top:20%;
+`;
---
+++ src/components/organisms/intro/index.ts
@@ -0,0 +1,1 @@
+export { default as IntroBody } from './Body';
