da82df472c5b6965defcbfd778034fb8cfbab0fc
jhpark <qkr13wlgus57@gmail.com>
2022-06-21 23:33:42 +0900

    feat(components): atoms add Layout

--- src/components/atoms/Layout.tsx
+++ src/components/atoms/Layout.tsx
@@ -1,7 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { NextSeo, NextSeoProps } from 'next-seo';
+interface Iprops extends NextSeoProps {
+  children: ReactNode;
+}
 
-export const Layout: FC<NextSeoProps> = ({ children, ...rest }) => (
+export const Layout: FC<Iprops> = ({ children, ...rest }) => (
   <>
     <NextSeo {...rest} />
     <>{children}</>
