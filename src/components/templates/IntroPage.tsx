6cf1a8858e13a44f06d44791ad1cc50e267c5301
jhpark <qkr13wlgus57@gmail.com>
2022-06-21 23:34:53 +0900

    feat(components): template add InfroPage

---
+++ src/components/templates/IntroPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Layout } from '@/components/atoms';
+import { IntroBody } from '@/components/organisms/intro';
+
+export const IntroPage: FC = () => {
+  return (
+      <Layout>
+          <IntroBody />
+      </Layout>
+  );
+};
