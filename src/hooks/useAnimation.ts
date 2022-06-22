1b686ca0877a110129a6086fe81e0bbb6eccede6
jhpark <qkr13wlgus57@gmail.com>
2022-06-22 21:08:03 +0900

    fix(hooks): change return data from percent to change rate

--- src/hooks/useAnimation.ts
+++ src/hooks/useAnimation.ts
@@ -1,16 +1,14 @@
 import { useAnimationTimer } from "./useAnimationTimer";
 import { easing } from "ts-easing";
 
-import type { EasingKey } from "@/types";
+import { EasingKey } from "@/types";
 
 export function useAnimation(
   easingKey: EasingKey = "linear",
   duration = 500,
   delay = 0,
-  count = 100
-) {
+): number {
   const elapsed = useAnimationTimer(duration, delay);
   const n = Math.min(1, elapsed / duration);
-  const result = Number(easing[easingKey](n).toFixed(2));
-  return Math.ceil(count * result);
+  return easing[easingKey](n)
 }
