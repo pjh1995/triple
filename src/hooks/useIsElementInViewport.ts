6fd75ba5349dff87b4c5ce069e09e485183d151b
jhpark <qkr13wlgus57@gmail.com>
2022-06-23 12:03:23 +0900

    chore(lint): lint fix

--- src/hooks/useIsElementInViewport.ts
+++ src/hooks/useIsElementInViewport.ts
@@ -1,25 +1,27 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react'
 
 interface Options extends IntersectionObserverInit {
-    once?: boolean
+  once?: boolean
 }
 export const useIsElementInViewport = (options: Options) => {
-    const elementRef = useRef(null);
-    const [isVisible, setIsVisible] = useState(false);
-    let observer: IntersectionObserver;
+  const elementRef = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+  let observer: IntersectionObserver
 
-    const callback = (entries: IntersectionObserverEntry[]) => {
-        const [entry] = entries;
-        if (options.once && entry.isIntersecting) observer.disconnect()
-        setIsVisible(entry.isIntersecting);
-    };
+  const callback = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries
+    if (options.once && entry.isIntersecting) {
+      observer.disconnect()
+    }
+    setIsVisible(entry.isIntersecting)
+  }
 
-    useEffect(() => {
-        observer = new IntersectionObserver(callback, options);
-        elementRef.current && observer.observe(elementRef.current);
+  useEffect(() => {
+    observer = new IntersectionObserver(callback, options)
+    elementRef.current && observer.observe(elementRef.current)
 
-        return () => observer.disconnect();
-    }, [elementRef, options]);
+    return () => observer.disconnect()
+  }, [elementRef, options])
 
-    return { elementRef, isVisible };
-};
+  return { elementRef, isVisible }
+}
